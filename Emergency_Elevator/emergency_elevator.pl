d6ace0ec9d03ca47fa383b019d8cd1f389bab4ec
mrazCODING <80523328+bviz6542@users.noreply.github.com>
2022-06-20 08:29:59 +0900

    Update emergency_elevator.pl

--- Emergency_Elevator/emergency_elevator.pl
+++ Emergency_Elevator/emergency_elevator.pl
@@ -12,19 +12,7 @@ elev에 담을 속성: 호수, 방향, 현재 층, 방문 리스트
 
 
 %%% 목표 층이 리스트 형태로 존재해야 함
-elev([Name,Direction,Now,Uplist,Downlist, Distance]):-
-    print(Name),Direction =:= 1 -> printElevU(Now);printElevD(Now),
-    %%% 여기서 우선순위 큐로 순서 재지정해줘야 함
-    Direction =:= 1 -> Distance is Uplist-Now;Distance is Now-Downlist,
-    (Distance =:= 0, Direction =:= 1)-> print(' ~open~'),Direction is 0,
-    %%% 큐에서 팝해야 함, 일단은 제일 높은 곳에 멈췄다는 가정
-    (Distance =:= 0, Direction =:= 0)-> print(' ~open~'),Direction is 1,
-    (Distance > 0, Direction =:= 1)-> Now is Now+1,
-    (Distance > 0, Direction =:= 0)-> Now is Now-1; Now is Now.
-
-printState([A1,A2,A3,A4,A5,A6],[B1,B2,B3,B4,B5,B6],[C1,C2,C3,C4,C5,C6]):-
-    print('floor: 1 2 3 4 5 6 7'),nl,
-    
+elev([Name,Direction,Now,Uplist,Downlist, Distance],[X1,X2,X3,X4,X5,X6]):-
     print(A1),(A2 =:= 1 -> printElevU(A3);printElevD(A3)),
     %%% 여기서 우선순위 큐로 순서 재지정해줘야 함
     (A6 =:= 0 -> AA3 is A3, print(' $open$'),
@@ -33,31 +21,18 @@ printState([A1,A2,A3,A4,A5,A6],[B1,B2,B3,B4,B5,B6],[C1,C2,C3,C4,C5,C6]):-
     (A6 > 0 -> AA2 is A2,
         (A2 =:= 1-> AA3 is A3+1; AA3 is A3-1);!),
     (A2 =:= 1 -> AA6 is A4-A3-1; AA6 is A3-A5-1),
-    print([A1,AA2,AA3,A4,A5,AA6]),nl,
+    print([A1,AA2,AA3,A4,A5,AA6]),nl.
+
+printState([A1,A2,A3,A4,A5,A6],[B1,B2,B3,B4,B5,B6],[C1,C2,C3,C4,C5,C6]):-
+    print('floor: 1 2 3 4 5 6 7'),nl,
     
-    print(B1),(B2 =:= 1 -> printElevU(B3);printElevD(B3)),
-    %%% 여기서 우선순위 큐로 순서 재지정해줘야 함
-    (B6 =:= 0 -> BB3 is B3,print(' $open$'),
-        (B2 =:= 1-> BB2 is 0; BB2 is 1);!),nl,
-    %%% 큐에서 팝해야 함, 일단은 제일 높은 곳에 멈췄다는 가정
-    (B6 > 0 -> BB2 is B2,
-        (B2 =:= 1-> BB3 is B3+1; BB3 is B3-1);!),
-    (B2 =:= 1 -> BB6 is B4-B3-1; BB6 is B3-B5-1),
-    %print([B1,BB2,BB3,B4,B5,BB6]),nl,
-    
-    print(C1),(C2 =:= 1 -> printElevU(C3);printElevD(C3)),
-    %%% 여기서 우선순위 큐로 순서 재지정해줘야 함
-    (C6 =:= 0 -> CC3 is C3, print(' $open$'),
-        (C2 =:= 1-> CC2 is 0; CC2 is 1);!),nl,
-    %%% 큐에서 팝해야 함, 일단은 제일 높은 곳에 멈췄다는 가정
-    (C6 > 0 -> CC2 is C2,
-        (C2 =:= 1-> CC3 is C3+1; CC3 is C3-1);!),
-    (C2 =:= 1 -> CC6 is C4-C3-1; CC6 is C3-C5-1),
-    %print([C1,CC2,CC3,C4,C5,CC6]),nl,
+    elev([A1,A2,A3,A4,A5,A6],[NA1,NA2,NA3,NA4,NA5,NA6]),
+    elev([B1,B2,B3,B4,B5,B6],[NB1,NB2,NB3,NB4,NB5,NB6]),
+    elev([C1,C2,C3,C4,C5,C6],[NC1,NC2,NC3,NC4,NC5,NC6]),
     
     write('Type [floor_number,direction] or type keepgoing: '),read(X), process(X),
     %%% 여기서 최적의 엘리베이터 배정해줘야 함
-    printState([A1,AA2,AA3,A4,A5,AA6],[B1,BB2,BB3,B4,B5,BB6],[C1,CC2,CC3,C4,C5,CC6]).
+    printState([NA1,NA2,NA3,NA4,NA5,NA6],[NB1,NB2,NB3,NB4,NB5,NB6],[NC1,NC2,NC3,NC4,NC5,NC6]).
 
 process(keepgoing).
 process([X,Y]) :- X is X, Y is Y.
@@ -79,9 +54,40 @@ printElevU(5):-print(' 0 0 0 0 > 0 0').
 printElevU(6):-print(' 0 0 0 0 0 > 0').
 printElevU(7):-print(' 0 0 0 0 0 0 >').
 
-/*
-insert_sort_queue(State, [], [State]).
-insert_sort_queue(State, [H | T], [State, H | T]) :- precedes(State, H).
-insert_sort_queue(State, [H|T], [H | T_new]) :- insert_sort_queue(State, T, T_new).
-remove_sort_queue(First, [First|Rest], Rest).
-*/
+%%%
+empty_queue([]).
+enqueue(E, [], [E]).
+enqueue(E, [H|T], [H|Tnew]) :- enqueue(E, T, Tnew).
+dequeue(E, [E|T], T).
+dequeue(E, [E|T], _).
+member_queue(Element, Queue) :- member(Element, Queue).
+add_list_to_queue(List, Queue, Newqueue) :- append(Queue, List, Newqueue).
+%%%
+%insert_pq(State, [], [State]):- !.
+insert_pq(State, [H|Tail], [State, H|Tail]) :- precedes(State, H),!.
+insert_pq(State, [H|T], [H|Tnew]) :- insert_pq(State, T, Tnew).
+precedes(X, Y) :- X < Y.
+insert_list_pq([ ], L, L).
+insert_list_pq([State|Tail], L, New_L) :- insert_pq(State, L, L2), insert_list_pq(Tail, L2, New_L).
+%%%
+empty_set([]).
+member_set(E, S) :- member(E, S).
+delete_if_in_set(_, [], []).
+delete_if_in_set(E, [E|T], T):- !.
+delete_if_in_set(E, [H|T], [H|T_new]) :- delete_if_in_set(E, T, T_new), !.
+add_if_not_in_set(X, S, S) :- member(X, S), !.
+add_if_not_in_set(X, S, [X|S]).
+union([], S, S).
+union([H|T], S, S_new) :- union(T, S, S2),
+		       add_if_not_in_set(H, S2, S_new), !.
+subset([], _).
+subset([H|T], S) :- member_set(H, S), subset(T, S).
+intersection([], _, []).
+intersection([H|T], S, [H|S_new]) :- member_set(H, S),
+	intersection(T, S, S_new), !.
+intersection([_|T], S, S_new) :- intersection(T, S, S_new), !.
+set_difference([], _, []).
+set_difference([H|T], S, T_new) :- member_set(H, S),
+	set_difference(T, S, T_new), !.
+set_difference([H|T], S, [H|T_new]) :- set_difference(T, S, T_new), !.
+equal_set(S1, S2) :- subset(S1, S2), subset(S2, S1).
