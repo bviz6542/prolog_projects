9ddb9eefa591db156851cc0d11307b605e5a2db1
mrazCODING <80523328+bviz6542@users.noreply.github.com>
2022-06-19 13:29:26 +0900

    Update try_assignment.pl

--- Emergency_Elevator/try_assignment.pl
+++ Emergency_Elevator/try_assignment.pl
@@ -3,10 +3,9 @@ cube:-write('hey: '), read(Number), C is Number*Number/3, print(C).
 elev(X,Y):- (X =:= 1, Y =:= 0) -> print(X), print('1-1'), 
     X < 2 -> print('2-1'); print('2-2').
 
-count(C):-
-    (C>5 -> print('erwe'),Y is 66; Y is 77),
-    print(Y),
+count(A1,A2,A3):-
+    (A1 =:= 1 -> (A2 < 1 -> AA6 is 999;!),(A2 > 1 -> AA6 is 777+A3;!),print('ho ');!),
+    (A1 =\= 1 -> (A2 < 1 -> AA6 is 666;!),(A2 > 1 -> AA6 is 111+A3;!),print('ho ');!),
+    print(AA6),
     read(X),print(X),
-    count(Y).
-
-% ?- count(4)
+    count(A1,A2,AA6).
